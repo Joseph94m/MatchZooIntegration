8965f2fdfc77b2c0a7ec7fc462b385b6c53f56b9
Joseph <moukarzeljoseph@gmail.com>
2018-07-07 13:06:31 +0100

    Largely optimized MatchZooDocumentRepresentor. Time to process went down from 2 hours to 30 seconds

--- IRcourseHM-master/src/main/java/uk/ac/gla/dcs/dsms/MatchZooDocumentRepresentor.java
+++ IRcourseHM-master/src/main/java/uk/ac/gla/dcs/dsms/MatchZooDocumentRepresentor.java
@@ -9,11 +9,18 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Arrays;
+import java.util.Map;
 import org.terrier.indexing.tokenisation.Tokeniser;
+import org.terrier.structures.BitIndexPointer;
+import org.terrier.structures.DocumentIndex;
 import org.terrier.structures.Index;
 import org.terrier.structures.Lexicon;
 import org.terrier.structures.LexiconEntry;
 import org.terrier.structures.MetaIndex;
+import org.terrier.structures.PostingIndex;
+import org.terrier.structures.postings.BlockPosting;
+import org.terrier.structures.postings.IterablePosting;
 
 /**
  *
@@ -38,43 +45,76 @@ public class MatchZooDocumentRepresentor {
 
     private void represent(int[] docids) throws IOException {
         MetaIndex mi = index.getMetaIndex();
-        Lexicon<String> lex = index.getLexicon();
         StringBuilder sb;
-        StringBuilder tmp_body;
-        int token_count;
+        IterablePosting postings;
+        int[] text;
+        int doc_length;
+        int term_id;
         for (int i = 0; i < docids.length; ++i) {
             if (iterated[docids[i]] == true) {
                 continue;
             }
             iterated[docids[i]] = true;
-            token_count = 0;
-            tmp_body = new StringBuilder();
             sb = new StringBuilder();
-            String body = mi.getItem("body", docids[i]);
-            String[] tokenized_body = tk.getTokens(body);
+
+            PostingIndex<?> di = index.getDirectIndex();
+            DocumentIndex doi = index.getDocumentIndex();
+            doc_length = doi.getDocumentLength(docids[i]);
+
+            if (doc_length > 200) {
+                text = new int[200];
+                doc_length = 200;
+            } else {
+                text = new int[doc_length];
+            }
+
+            postings = di.getPostings(doi.getDocumentEntry(docids[i]));
+            while (postings.next() != IterablePosting.EOL) {
+                term_id = postings.getId();
+                int[] pos = ((BlockPosting) postings).getPositions();
+
+                for (int position : pos) {
+                    if (position < 200) {
+                        text[position] = term_id;
+                    }
+                }
+
+            }
             sb.append("D");
             sb.append(docids[i]);
             sb.append(" ");
-            for (String t : tokenized_body) {
-                if (token_count == docSize) {
-                    break;
-                }
-                LexiconEntry le = lex.getLexiconEntry(t);
-                if (le == null) {
-                } else {
-                    ++token_count;
-                    tmp_body.append(" ");
-                    tmp_body.append(le.getTermId());
-                }
+            sb.append(doc_length);
+            for (int t : text) {
+                sb.append(" ");
+                sb.append(t);
             }
-            
-            sb.append(token_count);
-            sb.append(tmp_body.toString());
             sb.append('\n');
             bw.write(sb.toString());
             bw.flush();
-            
 
+//            String body = mi.getItem("body", docids[i]);
+//            String[] tokenized_body = tk.getTokens(body);
+//            sb.append("D");
+//            sb.append(docids[i]);
+//            sb.append(" ");
+//            for (String t : tokenized_body) {
+//                if (token_count == docSize) {
+//                    break;
+//                }
+//                LexiconEntry le = lex.getLexiconEntry(t);
+//                if (le == null) {
+//                } else {
+//                    ++token_count;
+//                    tmp_body.append(" ");
+//                    tmp_body.append(le.getTermId());
+//                }
+//            }
+//
+//            sb.append(token_count);
+//            sb.append(tmp_body.toString());
+//            sb.append('\n');
+//            bw.write(sb.toString());
+//            bw.flush();
         }
     }
 
