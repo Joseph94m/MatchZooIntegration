40b4012c036503689091303a04bb0b6d55b51cb1
Joseph <moukarzeljoseph@gmail.com>
2018-08-02 13:16:27 +0100

    Added model file

---
+++ IRcourseHM-master/src/main/java/uk/ac/gla/dcs/dsms/MZModel.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.ac.gla.dcs.dsms;
+
+import java.io.IOException;
+import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.deeplearning4j.nn.modelimport.keras.InvalidKerasConfigurationException;
+import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
+import org.deeplearning4j.nn.modelimport.keras.UnsupportedKerasConfigurationException;
+
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
+/**
+ *
+ * @author Joseph
+ */
+public class MZModel {
+
+    public static void main(String[] args) throws IOException, UnsupportedKerasConfigurationException, InvalidKerasConfigurationException {
+        //  KerasModelImport.importKerasModelAndWeights("C:\\Users\\Joseph\\Desktop\\Studies\\Project\\MatchZoo\\examples\\Robust\\weights\\nmws.Robust.444.50.false.254.weights.1.model", "C:\\Users\\Joseph\\Desktop\\Studies\\Project\\MatchZoo\\examples\\Robust\\weights\\nmws.Robust.444.50.false.254.weights.1");
+        //KerasModelImport.importKerasModelAndWeights("C:\\Users\\Joseph\\Desktop\\Studies\\Project\\MatchZoo\\examples\\Robust\\weights\\nmws.Robust.444.50.false.254.weights.1.model");
+        KerasModelImport.importKerasModelAndWeights("C:\\Users\\Joseph\\Desktop\\Studies\\Project\\MatchZoo\\examples\\Robust\\weights\\knrm.Robust.444.50.false.50.weights.1.model", false);
+    }
+}
